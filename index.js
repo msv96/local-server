7b36c3be6e9921f8f9d72eff73c6eea5a90e245c
msv96 <msrini1100@gmail.com>
2021-09-04 11:40:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get("/", function (req, res) {
 	res.send("Hello. Welcome to the App");
 });
 
+app.get("/users", function (req, res) {
+	res.send("All users are safe");
+});
+
+app.get("/products", function (req, res) {
+	res.send("<h1>All products have been sold out</h1>");
+});
+
 app.listen(3030, function () {
 	console.log("The Server is listening in port : 3030");
 });
